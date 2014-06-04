dffd05751d4f397f0b3f52e43fe9e5a85b40e389
Drew <123and@gmail.com>
2014-06-04 14:35:01 -0700

    Add styling to show, edit views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   devise_for :views
   devise_for :users
   resources :comments
-
   root "questions#index"
+
+  resources :questions do
+    resources :favorites, only: [:create, :destroy, :update]
+    resources :votes, only: [:create, :update, :destroy]
+    resources :answers
+    post :search, on: :collection
+  end
 
   # get "/about_us"           => "home#about"
   # get "/faq"                => "faq#index"
@@ -19,11 +25,5 @@ Rails.application.routes.draw do
   # match "/questions/:id"    => "questions#update", via: [:put, :patch]
   # delete "/questions/:id"   => "questions#destroy"
 
-  resources :questions do
-    resources :favorites, only: [:create, :destroy, :update]
-    resources :votes, only: [:create, :update, :destroy]
-    resources :answers
-    post :search, on: :collection
-  end
 
 end
